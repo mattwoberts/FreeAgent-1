f59fd6fe66f208bb2b4389a5c680441afc4ced00
mattwoberts <roberts.mattroberts@gmail.com>
2017-09-25 13:59:54 +0100

    Added a test for invoicing a new contact

--- src/FreeAgent.Tests/InvoiceTests.cs
+++ src/FreeAgent.Tests/InvoiceTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FreeAgent.Helpers;
 using NUnit.Framework;
 using FreeAgent.Model;
 
@@ -66,6 +67,87 @@ namespace FreeAgent.Tests
         [Ignore("")]
         public void UpdateInvoice()
         {
+
+
+        }
+
+
+        [Test]
+        public async Task CanRaiseInvoiceAndBankExplanation()
+        {
+            // Create a rnadom contact:
+            var source = Helper.NewContact();
+            var contact = await this.Client.CreateContactAsync(source);
+            Assert.NotNull(contact.Url);
+
+            // Create an invoice for this contact
+            var items = new List<InvoiceItem>
+            {
+                new InvoiceItem()
+                {
+                    ItemType = InvoiceItemType.NoUnit,
+                    Price = 100,
+                    Description = "Some things",
+                    Quantity = 100,
+                    SalesTaxRate = 20
+                }
+            };
+
+            var invoice = new Invoice
+            {
+                Contact = contact.Url,
+                DatedOn = DateTime.Now.AddDays(-1),
+                DueOn = DateTime.Now.AddDays(1),
+                PaymentTermsInDays = 25,
+                InvoiceItems = items,
+                Currency = "GBP",
+                Comments = "Thanks, all done."
+            };
+
+            try
+            {
+                invoice = await this.Client.CreateInvoice(invoice);
+
+            }
+            catch (FreeAgentException e)
+            {
+                Console.WriteLine(e.InnerException.ToString());
+            }
+            Assert.NotNull(invoice.Url);
+
+            // Now we need to create an email for the invoice
+            var email = new InvoiceEmail
+            {
+                Email =
+                    new InvoiceEmailDetail
+                    {
+                        Body = "Hello there, here you are.",
+                        Subject = "Your invoice",
+                    }
+            };
+
+            await this.Client.CreateInvoiceEmail(invoice.GetId(), email);
+
+            var bankAccount = await Client.GetBankAccountsAsync();
+
+            // Create a bank account explanation for the new invoice now.
+            var explanation = new BankTransactionExplanation
+            {
+                BankAccount = bankAccount.First().Url,
+                DatedOn = DateTime.Now,
+                PaidInvoice = invoice.Url,
+                GrossValue = invoice.TotalValue,
+                ForeignCurrencyValue = 0
+            };
+
+            explanation = await this.Client.CreateBankTransactionExplanationAsync(explanation);
+
+            Assert.NotNull(explanation.Url);
+
+            // And for my final trick, I shall now get the PDF of that invoice.
+            var pdf = await this.Client.GetInvoicePdfAsync(invoice.GetId());
+
+            Assert.NotNull(pdf?.Content);
 
 
         }
